47c706d1a6653371589686a8cb2e6b60d1154761
이민우 <minwoo9629@naver.com>
2022-01-18 20:35:47 +0900

    개인 제출 폴더 생성

---
+++ Lee/BOJ11050.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class temporary {
+    private static int n, k, result;
+
+    public static void dfs(int start, int k) {
+        if (k == 0) {
+            result += 1;
+            return;
+        }
+        for (int num = start; num < n + 1; num++) {
+            dfs(num + 1, k - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        dfs(1, k);
+        System.out.println(result);
+    }
+}
