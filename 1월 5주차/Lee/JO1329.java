becd73ca53d255c0954a74ed245cc06a9c80e651
minwoo9629 <minwoo9629@gmail.com>
2022-01-26 21:14:52 +0900

    정올 : 1329 별삼각형3

---
+++ 1월 5주차/Lee/JO1329.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class JO1329 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n > 100 | n % 2 == 0) {
+            System.out.println("INPUT ERROR!");
+        } else {
+            for (int i = 0; i < n; i++) {
+                // 위
+                if (i <= n / 2) {
+                    // 공백
+                    for (int j = 0; j < i; j++) {
+                        System.out.print(" ");
+                    }
+                    // 별 찍기
+                    for (int j = 0; j < 1 + 2 * i; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+
+                }
+                // 아래
+                else {
+                    // 공백
+                    for (int j = 0; j < 6 - i; j++) {
+                        System.out.print(" ");
+                    }
+                    // 별 찍기
+                    for (int j = 0; j < 1 + 2 * (6 - i); j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
