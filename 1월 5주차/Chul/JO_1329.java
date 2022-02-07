730520907c3298ed295f6a2edd80e64a763c8446
이정철 <ljc9301@naver.com>
2022-02-07 23:13:06 +0900

    정올 : 1329 별삼각형3

---
+++ 1월 5주차/Chul/JO_1329.java
@@ -0,0 +1,32 @@
+package JUNGOL;
+
+import java.util.Scanner;
+
+public class JO_1329 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+         
+        if(N % 2 == 0 || N > 100 || N < 1){
+            System.out.println("INPUT ERROR!");
+            return;
+        }
+        for(int i = 0 ; i < N/2+1; ++i)
+        {
+            for(int j = 0; j < i; ++j)
+                System.out.print(" ");
+            	for(int j = 0 ; j < 2*i+1; ++j)
+            		System.out.print("*");
+            System.out.println();
+        }
+        
+        for(int i = N/2-1; i >= 0; --i)
+        {
+            for(int j = 0; j < i; ++j)
+                System.out.print(" ");
+            	for(int j = 0 ; j < 2*i+1; ++j)
+            		System.out.print("*");
+            System.out.println();
+        }
+    }
+}
