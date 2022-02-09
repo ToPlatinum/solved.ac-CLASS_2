aae45b224a010d64d4c8262c6d2ea452504704ae
dladndrbs <dladndrbs@naver.com>
2022-02-10 00:37:56 +0900

    백준:15652 N과M(4)

---
+++ N과M챌린지/Im/BJ_15652.java
@@ -0,0 +1,33 @@
+package silver_3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJ_15652 {
+
+	static int N, M;
+	static int[] sel;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sel = new int[M];
+		Combination(1, 0);
+
+	}
+
+	private static void Combination(int start, int k) {
+		if (k == M) {
+			System.out.println(Arrays.toString(sel).replaceAll("[\\]\\,\\[]", ""));
+			return;
+		}
+
+		for (int i = start; i <= N; i++) {
+			sel[k] = i;
+			Combination(i, k + 1);
+		}
+
+	}
+
+}
