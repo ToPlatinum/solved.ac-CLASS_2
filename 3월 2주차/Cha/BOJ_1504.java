6523cc7c3399c1cca13d1ce49999dc9439b9c186
차송희 <chasonghui@naver.com>
2022-03-19 14:43:58 +0900

    백준 : 1504 특정한 최단 경로

--- 3월 2주차/Cha/BOJ_1504.java
+++ 3월 2주차/Cha/BOJ_1504.java
@@ -9,23 +9,22 @@ import java.util.Scanner;
 
 // Node클래스 맨들기
 class Node implements Comparable<Node>{
-    int vertex;//도착지
+    int end;//도착지
     int weight;//가중치
 
-    Node(int vertex, int weight){
-        this.vertex = vertex;
+    Node(int end, int weight){
+        this.end = end;
         this.weight = weight;
     }
     @Override
     public int compareTo(Node o) {
-        return this.weight -o.weight;
+        return weight -o.weight;
     }
 }
 class BOJ_1504{
     static int N,E;
-    static ArrayList<ArrayList<Node>> adj;//인접 리스트를 맨들자
-    static int[] distance;//최단거리 저장할 배열
-    static int min = Integer.MAX_VALUE;
+    static ArrayList<Node>[] adj;//인접 리스트를 맨들자
+    static int INF = 200000000;//최단거리 초기화 할 값
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -33,23 +32,23 @@ class BOJ_1504{
         E = sc.nextInt();//간선의 개수
 
         //인접리스트 생성
-        adj = new ArrayList<>();
-        for (int i = 0; i < N+1; i++) {
-            adj.add(new ArrayList<>());
+        adj = new ArrayList[N+1];
+        for (int i = 1; i < N+1; i++) {
+            adj[i] = new ArrayList<>();
         }
 
-        distance = new int[N+1];
 
         //양방향 인접 리스트 
         for (int i = 0; i < E; i++) {
+            //간선 정보 입력
             int from = sc.nextInt();
             int to = sc.nextInt();
             int weight = sc.nextInt();
 
             // from to 가중치
-            adj.get(from).add(new Node(to,weight));
+            adj[from].add(new Node(to,weight));
             // to from 가중치
-            adj.get(to).add(new Node(from,weight));
+            adj[to].add(new Node(from,weight));
         }
 
         // 꼭 거쳐야 되는 정점 2개
@@ -57,20 +56,22 @@ class BOJ_1504{
         int b = sc.nextInt();
 
         //경우 2가지
-        long ans1=0, ans2=0;
+        int ans1=0;
+        int ans2=0;
 
-        // 시작 - a - b - 도착
+        //1번정점에서 N번 정점으로 가는 최단거리
+        // 1 - a - b - N
         ans1 += sol(1,a );
         ans1 += sol(a, b);
         ans1 += sol(b, N);
 
-        // 시작 - b - a - 도착
-        ans2 += sol(1,a );
+        // 1 - b - a - N
+        ans2 += sol(1,b );
         ans2 += sol(b,a );
         ans2 += sol(a,N );
 
         long answer =0;
-        if(Math.min(ans1,ans2)>=min){
+        if(ans1>=INF && ans2>=INF){
             answer = -1;
 
         }else{
@@ -82,35 +83,38 @@ class BOJ_1504{
         
     }
 
-    public static int sol(int from,int to){
+    // 다익스트라 알고리즘
+    public static int sol(int start, int end) {
+        boolean[] check=new boolean[N+1];
+        int[] dist = new int[N+1];
+
+        Arrays.fill(dist, INF);
+        Arrays.fill(check, false);
+
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        //시작위치로 가는 최단경로 0
-        pq.add(new Node(from,0));//큐에 넣기
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
 
-        Arrays.fill(distance,min);
-        distance[from] =0;
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            int curN = current.end;
+            int curW = current.weight;
 
-        while(!pq.isEmpty()){
-            // 최단거리가 가장짧은 노드 꺼내
-            Node node = pq.poll();
-            int now = node.vertex;//현재 노드
-            int weight = node.weight;//현재 노드 비용
-            
-            //현재 가중치가 더 크면 갱신X
-            if(distance[now]<weight){
-                continue;
-            }
-            //현재 노드랑 연결된 다른 인접노드들 확인
-            for (int i = 0; i < adj.get(now).size(); i++) {
-                int dist = distance[now]+adj.get(now).get(i).weight;
-                // 현재노드 -> 다른노드의 비용이 더 적을때
-                if(dist<distance[adj.get(now).get(i).vertex]){
-                    distance[adj.get(now).get(i).vertex]=dist;
-                    pq.offer(new Node(adj.get(now).get(i).vertex,dist));
+            if (check[curN]==false) check[curN] = true;
+            else continue;
+
+            for (int i=0;i<adj[curN].size();i++) {
+                int nextN = adj[curN].get(i).end;
+                int nextW = adj[curN].get(i).weight;
+                if (!check[nextN] && dist[nextN] > dist[curN] + nextW) {
+                    dist[nextN] =dist[curN] + nextW;
+                    pq.add(new Node(nextN, dist[nextN]));
                 }
             }
+
         }
-        return distance[to];
+
+        return dist[end];
     }
 }
 
